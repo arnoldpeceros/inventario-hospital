a492ee8cdf1ab75999fdfe24d86ad4b82e5e4678
Arnold Peceros <arnoldpeceros68@gmail.com>
2025-11-18 23:14:30 -0500

    feat: configuración inicial del proyecto Angular con Firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,24 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideFirebaseApp(() => initializeApp({
+      projectId: "inventario-hospital-arnold",
+      appId: "1:486280479526:web:823cc1fe5759905a2137e7",
+      storageBucket: "inventario-hospital-arnold.firebasestorage.app",
+      apiKey: "AIzaSyAxVMMNlBDUXR9_6pqCeBeJgYfBTNefqSY",
+      authDomain: "inventario-hospital-arnold.firebaseapp.com",
+      messagingSenderId: "486280479526"
+    })),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore())
   ]
 };
