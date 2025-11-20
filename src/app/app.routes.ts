da7a9843b6a7ed51232ea08cb99f567669c68301
Arnold Peceros <arnoldpeceros68@gmail.com>
2025-11-19 22:11:44 -0500

    componentes principales y sistema de navegación con rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
+import { Login } from './pages/login/login';
+import { Inicio } from './pages/inicio/inicio';
+import { Productos } from './pages/productos/productos';
+import { Movimientos } from './pages/movimientos/movimientos';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  // Si entras a la raíz, te manda al login
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  
+  // Ruta del login
+  { path: 'login', component: Login },
+  
+  // Ruta de inicio (dashboard)
+  { path: 'inicio', component: Inicio },
+  
+  // Ruta de productos
+  { path: 'productos', component: Productos },
+  
+  // Ruta de movimientos
+  { path: 'movimientos', component: Movimientos },
+  
+  // Si escriben algo que no existe, vuelve al login
+  { path: '**', redirectTo: 'login' }
+];
